17c2910882c8f0834e3b3978d8543adcde604e53
RussianBlue25 <horota2@outlook.jp>
2020-09-13 12:39:47 +0900

    add return

--- main.c
+++ main.c
@@ -3,16 +3,26 @@
 int main(void) {
   int i = 1;
 
-  printf("  .intel_systax noprefix\n");
-  printf("  .globl  main\n");
+  printf("    .text\n");
+  printf("    .globl  main\n");
+  printf("    .type main, @function\n");
 
   printf("main:\n");
-  printf("  pushq  %%rbp\n");
-  printf("  movq  %%rsp, %%rbp\n");
-  printf("  movl  $%d, %%eax\n", i);
-  printf("  popq  %%rbp\n");
-  printf("  ret\n");
-  //printf("");
+  printf(".LFB0:\n");
+  printf("    .cfi_startproc\n");
+  printf("    pushq  %%rbp\n");
+  printf("    .cfi_def_cfa_offset 16\n");
+  printf("    .cfi_offset 6, -16\n");
+  printf("    movq  %%rsp, %%rbp\n");
+  printf("    .cfi_def_cfa_register 6\n");
+  printf("    movl  $%d, %%eax\n", i);
+  printf("    popq  %%rbp\n");
+  printf("    .cfi_def_cfa 7, 8\n");
+  printf("    ret\n");
+  printf("    .cfi_endproc\n");
+
+  printf("LFE0:\n");
+  printf("    .size  main, .-main\n");
   //printf("");
   //printf("");
   //printf("");
--- test.s
+++ test.s
@@ -1,8 +1,18 @@
-  .intel_systax noprefix
-  .globl  main
+    .text
+    .globl  main
+    .type main, @function
 main:
-  pushq  %rbp
-  movq  %rsp, %rbp
-  movl  $1, %eax
-  popq  %rbp
-  ret
+.LFB0:
+    .cfi_startproc
+    pushq  %rbp
+    .cfi_def_cfa_offset 16
+    .cfi_offset 6, -16
+    movq  %rsp, %rbp
+    .cfi_def_cfa_register 6
+    movl  $1, %eax
+    popq  %rbp
+    .cfi_def_cfa 7, 8
+    ret
+    .cfi_endproc
+LFE0:
+    .size  main, .-main
